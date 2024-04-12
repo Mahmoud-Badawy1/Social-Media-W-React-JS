0f6ab84114389f8f65fa488243b0b34621bbfc3f
Mahmoud-Badawy1 <mahmoudbadawy10h@gmail.com>
2024-04-12 19:58:48 +0200

    fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store/store'; // Ensure you have the correct path to your store configuration
-import './index.css';
+import store from './redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
